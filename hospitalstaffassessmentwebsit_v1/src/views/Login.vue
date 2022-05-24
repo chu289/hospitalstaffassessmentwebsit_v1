87f7a3ee8b87bcc4d14e23620e263acbb47ad82d
chu289 <asd0903a@gmail.com>
2022-05-24 21:55:32 +0800

    後台人員登入

--- hospitalstaffassessmentwebsit_v1/src/views/Login.vue
+++ hospitalstaffassessmentwebsit_v1/src/views/Login.vue
@@ -13,14 +13,13 @@
 <script>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   setup() {
     const username = ref("");
     const password = ref("");
     const router = useRouter();
-    
 
     function send() {
       console.log(Date.now());
@@ -31,31 +30,57 @@ export default {
       //   password.value = "";
       // }
       console.log(username.value);
-      console.log(password.value)
-      axios.
-        post('https://hospitalstaffassessmentserver.azurewebsites.net:443/login', {
-          ID:username.value,
-          password:password.value
-
-        })
-      .then((res) => {
-        console.log(res.data);
-        if (res.data.loginFlag === 1) {
-          console.log(res.data.employee_ID);
-          router.push({name:"users",params:{userID:res.data.employee_ID,id:res.data.employee_ID}});
-          // router.push({ path: '/users/' + res.data.employee_ID});
-        } else if(res.data.loginFlag === 0){
-          alert("帳號或密碼錯誤!");
-          password.value = "";
-        } else if(res.data.loginFlag === 2){
-          
-          alert("現在不是您的上班時間唷~");
-          //  router.push({ path: "/users/" });
-          router.push({name:"users",params:{userID:res.data.employee_ID,id:res.data.employee_ID}});
-          // router.push({ path: '/users/' + res.data.employee_ID});
-          
-        }       
-      })
+      console.log(password.value);
+      axios
+        .post(
+          "https://hospitalstaffassessmentserver.azurewebsites.net:443/login",
+          {
+            ID: username.value,
+            password: password.value,
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.loginFlag === 1) {
+            console.log(res.data.employee_ID);
+            router.push({
+              name: "users",
+              params: {
+                userID: res.data.employee_ID,
+                id: res.data.employee_ID,
+              },
+            });
+            // router.push({ path: '/users/' + res.data.employee_ID});
+          } else if (res.data.loginFlag === 0) {
+            setTimeout(function () {
+              alert("帳號或密碼錯誤!");
+            }, 0);
+            password.value = "";
+          } else if (res.data.loginFlag === 2) {
+            setTimeout(function () {
+              alert("現在不是您的上班時間唷~");
+            }, 0);
+            //  router.push({ path: "/users/" });
+            router.push({
+              name: "users",
+              params: {
+                userID: res.data.employee_ID,
+                id: res.data.employee_ID,
+              },
+            });
+            // router.push({ path: '/users/' + res.data.employee_ID});
+          } else if (res.data.loginFlag === 3) {
+            setTimeout(function () {
+              alert("後臺員工登入");
+            }, 0);
+            router.push({
+              name: "backstage",
+              params: {
+                userID: res.data.employee_ID,
+              },
+            });
+          }
+        });
     }
 
     return {
